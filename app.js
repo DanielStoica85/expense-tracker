8be8144d30dd9d844cb2f6b7e262c9d16d907ef1
Daniel <dansto13@yahoo.com>
2018-03-18 01:48:40 +0100

    Add data structures.

--- app.js
+++ app.js
@@ -3,3 +3,72 @@
 $(document).ready(function () {
     $('select').material_select();
 });
+
+// Storage Controller
+
+// Item Controller
+const ItemController = (function () {
+
+    // Item (expense) constructor
+    const Item = function (id, category, name, amount) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.amount = amount;
+    }
+
+    // Data Structure / State
+    const data = {
+        items: [{
+            id: 0,
+            category: 'Housing',
+            name: 'Rent',
+            amount: 650
+        }, {
+            id: 0,
+            category: 'Restaurant',
+            name: 'Monelli',
+            amount: 100
+        }, {
+            id: 0,
+            category: 'Gas',
+            name: 'Gas March',
+            amount: 100
+        }],
+        currentItem: null,
+        totalExpenses: 0
+    }
+
+    // Public methods
+    return {
+        logData: function () {
+            return data;
+        }
+    }
+
+})();
+
+// UI Controller
+const UIController = (function () {
+
+    // Public methods
+    return {
+
+    }
+
+})();
+
+// App Controller
+const App = (function (ItemController, UIController) {
+
+    // Public methods
+    return {
+        init: function () {
+            console.log('Initializing app!');
+        }
+    }
+
+})(ItemController, UIController);
+
+// Initialize App
+App.init();
